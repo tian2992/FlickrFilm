818df44a1ec4b4cb0e8f9f09290cc9b025c094ef
Sebastian Oliva <code@sebastianoliva.com>
2017-06-15 16:25:40 -0600

    refresh

--- app.py
+++ app.py
@@ -8,15 +8,17 @@ app = Flask(__name__)
 import flickrapi
 
 USER_ID = "12983620@N00"
-API_KEY = "70ee1002979542fbca5710bba213f39b"
-flickr = flickrapi.FlickrAPI(API_KEY)
+APIKI = "70ee1002979542fbca5710bba213f39b"
+SICRET = "3c9741afa399e6ce"
+flickr = flickrapi.FlickrAPI(APIKI, SICRET)
 
 def build_url(photo, size):
     return "http://farm{0}.static.flickr.com/{1}/{2}_{3}_{4}.jpg".format(photo.get('farm'),photo.get('server'),
            photo.get('id'),photo.get('secret'),size)
 
 def fetchRolls():
-    photos = flickr.walk(user_id=USER_ID,tags="film",extras="tags")
+    '''Returns a dict with ids and list of pictures.'''
+    photos = flickr.walk(user_id=USER_ID, tags="film", extras="description,tags,machine_tags")
     rolls = {}
     for p in photos:
         tags = p.get('tags').split()
@@ -32,12 +34,12 @@ def fetchRolls():
 
 @app.route('/')
 def index():
-    rolls = fetchRolls()
-    return render_template('index.html',film=rolls,user=USER_ID,url_builder=build_url)
+    films = reversed(fetchRolls().values())
+    return render_template('index.html',film=films,user=USER_ID,url_builder=build_url)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
